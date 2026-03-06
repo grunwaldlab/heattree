d8f7bb980e5e12b182f3cee4b5c7e51e85839a32
Zachary S.L. Foster <zacharyfoster1989@gmail.com>
2026-03-06 13:57:22 -0800

    added Bansal et al. 2021 example data

--- R/dataset_documentation.R
+++ R/dataset_documentation.R
@@ -17,3 +17,38 @@ NULL
 #' @source From a core gene phylogeny of publicly available sequences
 #' @keywords data
 NULL
+
+
+#' A phylogeny from Bansal et al. 2021
+#'
+#' A core gene phylogeny from Bansal et al. 2021
+#' (https://doi.org/10.1016/j.ygeno.2021.09.021) included for use as an example
+#' data set. This is the parsed version of the included "bansal_2021_tree.nwk"
+#' file.
+#'
+#' @name bansal_2021_tree
+#' @format `ape::phylo`
+#' @source Bansal, K., Kumar, S., Kaur, A., Singh, A. and Patil, P.B., 2021.
+#'   Deep phylo-taxono genomics reveals Xylella as a variant lineage of plant
+#'   associated Xanthomonas and supports their taxonomic reunification along
+#'   with Stenotrophomonas and Pseudoxanthomonas. Genomics, 113(6),
+#'   pp.3989-4003.
+#' @keywords data
+NULL
+
+#' Isolate metadata from Bansal et al. 2021
+#'
+#' Isolate metadata from Bansal et al. 2021
+#' (https://doi.org/10.1016/j.ygeno.2021.09.021) included for use as an example
+#' data set. This is the parsed version of the included "bansal_2021_metadata.tsv"
+#' file.
+#'
+#' @name bansal_2021_metadata
+#' @format `data.frame`
+#' @source Bansal, K., Kumar, S., Kaur, A., Singh, A. and Patil, P.B., 2021.
+#'   Deep phylo-taxono genomics reveals Xylella as a variant lineage of plant
+#'   associated Xanthomonas and supports their taxonomic reunification along
+#'   with Stenotrophomonas and Pseudoxanthomonas. Genomics, 113(6),
+#'   pp.3989-4003.
+#' @keywords data
+NULL
